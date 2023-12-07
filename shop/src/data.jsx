1ac0106d3a599ef47d2da14d6a6c40b1f3f279d5
Peterngacha85 <peterngacha85@gmail.com>
2023-12-07 21:04:53 +0300

    under development

---
+++ shop/src/data.jsx
@@ -0,0 +1,66 @@
+const data = {
+    productItem: [
+       {
+        id: "1",
+        name: "Laptop bag.",
+        price: 550,
+        image: "images/belt.jpg",
+       },
+       {
+        id: "2",
+        name: "belt",
+        price: 250,
+        image: "images/belt.jpg",
+       },
+       {
+        id: "3",
+        name: "Camera",
+        price: 14500,
+        image: "images/camera.png",
+       },
+       {
+        id: "4",
+        name: "Cape",
+        price: 300,
+        image:"images/cape.webp",
+       },
+       {
+        id: "5",
+        name: "Laptop",
+        price: 45000,
+        image: "images/laptop.png",
+       },
+       {
+        id: "6",
+        name: "Phone",
+        price: 5500,
+        image: "images/phone.jpg",
+       },
+       {
+        id: "7",
+        name: "Shirt",
+        price: 800,
+        image: "images/shirt.webp",
+       },
+       {
+        id: "8",
+        name: "Shoe",
+        price: 2000,
+        image: "images/shoe.jpg",
+       },
+       {
+        id: "9",
+        name: "Sweat Shirt",
+        price: 999,
+        image: "images/sweat shirt.png",
+       },
+       {
+        id: "10",
+        name: "tv",
+        price: 25000,
+        image:"images/tv.png",
+       },
+    ]
+}
+
+export default data;
--- shop/src/pages/Shoping.jsx
+++ shop/src/pages/Shoping.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles/shop.css'
-import laptopImage from '../images/laptop.png';
+import data from '../data.jsx'
 
 const Shoping = () => {
+  const {productItem} =  data
+  const [test, setTest] = useState({})
+
   return (
     <div className='shop-container'>
-      <img src={laptopImage} alt= 'phone ..image'/>
+      {productItem.map((item)=>(
+       <div key={item.id}>
+          <img src={import(`../assets/images/images${item.image}`).then((image) => image.default)}/>
+          <h3>{item.name}</h3>
+          <h4>$ {item.price}</h4>
+          <button>Add to cart</button>
+       </div>
+      ))}
     </div>
   )
 }
