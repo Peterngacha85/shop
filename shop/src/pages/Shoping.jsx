7148eb9a22e22988f4b87b3218dbb5657fc2bfd3
Peterngacha85 <peterngacha85@gmail.com>
2023-12-07 10:44:19 +0300

    under development

--- shop/src/pages/Shoping.jsx
+++ shop/src/pages/Shoping.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './styles/shop.css'
+import laptopImage from '../images/laptop.png';
 
 const Shoping = () => {
   return (
     <div className='shop-container'>
-      <img src='/shop/src/images/laptop.png' alt= 'phone ..image'/>
+      <img src={laptopImage} alt= 'phone ..image'/>
     </div>
   )
 }
